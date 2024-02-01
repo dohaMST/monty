2282d416bff91f824ca8186c182d5e0f27bb2d05
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-01 08:09:57 -0800

    handle main01

--- monty.c
+++ monty.c
@@ -3,22 +3,23 @@
 char *val;
 
 /**
- * main - Function that run our interpreter
- * @argc: Argument Count
- * @argv: Argument vector
- * Return: .
+* main - the main function
+* @argc: the count of arguments
+* @argv: the array of arguments
+* Return: 0 for success
 */
+
 int main(int argc, char const *argv[])
 {
 	char *line = NULL, *line_te, *instruction;
-	size_t len = 0;
-	ssize_t read;
-	FILE *file;
-	unsigned int compt = 0;
+	size_t size = 0;
+	ssize_t x;
+	FILE *theFile;
+	unsigned int i = 0;
 	int idx;
-	instruction_t opcode[] = {{"push", push}, {"pall", pall}, {"pint", pint},
+	instruction_t opArray[] = {{"push", push}, {"pall", pall}, {"pint", pint},
 	{"pop", pop}, {"swap", swap}, {"add", add},{"nop", nop},{"sub", sub}};
-	stack_t *stack = NULL;
+	stack_t *list = NULL;
 
 	if (argc != 2)
 	{
@@ -28,8 +29,8 @@ int main(int argc, char const *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	if (file == NULL)
+	theFile = fopen(argv[1], "r");
+	if (theFile == NULL)
 	{
 		/*file_error(argv[1]);*/
 		/*added*/
@@ -38,31 +39,31 @@ int main(int argc, char const *argv[])
 	}
 	while (1)
 	{
-		compt++;
-		read = getline(&line, &len, file);
-		if (read == -1)
+		i++;
+		x = getline(&line, &size, theFile);
+		if (x == -1)
 			break;
 		line_te = handle_new_line(line);
 		if (strcmp(line_te, "\n") == 0 || line_te[0] == '#')
 			continue;
 		instruction = strtok(line_te, TOK_DELIM);
 		val = strtok(NULL, TOK_DELIM);
-		idx = opcode_(instruction, opcode);
+		idx = opcode_(instruction, opArray);
 		if (idx >= 0)
-			opcode[idx].f(&stack, compt);
+			opArray[idx].f(&list, i);
 		else
 		{
-			handle_free_list(&stack);
+			handle_free_list(&list);
 			/*instr_error(compt, line_te, line);*/
 			/*added*/
-			fprintf(stderr, "L%d: unknown instruction %s\n", compt, line_te);
+			fprintf(stderr, "L%d: unknown instruction %s\n", i, line_te);
 			free(line);
 			exit(EXIT_FAILURE);
 
 		}
 	}
 	free(line);
-	handle_free_list(&stack);
-	fclose(file);
+	handle_free_list(&list);
+	fclose(theFile);
 	return (0);
 }
