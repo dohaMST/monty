e72ab9e85844d36601575e1803830e6759f3bf69
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-01 16:25:22 -0800

    task7 v2

--- tas07.c
+++ tas07.c
@@ -7,22 +7,24 @@
  */
 void handle_div(stack_t **head, unsigned int ln)
 {
-	int sum;
+	int div;
 
 	if (head == NULL || *head == NULL || (*head)->next == NULL)
 	{
+		handle_free_list(head);
 		fprintf(stderr, "L%d: can't div, stack too short\n", ln);
 		exit(EXIT_FAILURE);
 	}
 
 	if ((*head)->n == 0)
 	{
+		handle_free_list(head);
 		fprintf(stderr, "L%d: division by zero\n", ln);
 		exit(EXIT_FAILURE);
 	}
 	(*head) = (*head)->next;
-	sum = (*head)->n / (*head)->prev->n;
-	(*head)->n = sum;
+	div = (*head)->n / (*head)->prev->n;
+	(*head)->n = div;
 	free((*head)->prev);
 	(*head)->prev = NULL;
 }
