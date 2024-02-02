e2ad03309dff7fed23b6c6d69728df6531f6b8a2
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-01 17:52:23 -0800

    edit task07

--- tas07.c
+++ tas07.c
@@ -9,6 +9,7 @@ void handle_div(stack_t **head, unsigned int ln)
 {
 	int div;
 
+	/*check if the stack is less than 2 elements*/
 	if (head == NULL || *head == NULL || (*head)->next == NULL)
 	{
 		handle_free_list(head);
@@ -16,6 +17,7 @@ void handle_div(stack_t **head, unsigned int ln)
 		exit(EXIT_FAILURE);
 	}
 
+	/*Checking for division by zero*/
 	if ((*head)->n == 0)
 	{
 		handle_free_list(head);
@@ -23,7 +25,9 @@ void handle_div(stack_t **head, unsigned int ln)
 		exit(EXIT_FAILURE);
 	}
 	(*head) = (*head)->next;
+	/*calculate the division*/
 	div = (*head)->n / (*head)->prev->n;
+	/*update the value of the current node*/
 	(*head)->n = div;
 	free((*head)->prev);
 	(*head)->prev = NULL;
