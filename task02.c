6981d8199d1f6351d9ec30e64ba5a72c5a955d60
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-01 18:05:24 -0800

    task02

--- task02.c
+++ task02.c
@@ -9,13 +9,16 @@ void handle_pop(stack_t **head, unsigned int ln)
 {
 	stack_t *tmp;
 
+	/*check if the stack is empty*/
 	if (head == NULL || *head == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", ln);
 		exit(EXIT_FAILURE);
 	}
 	tmp = *head;
+	/*move the head pointer to the next node*/
 	*head = tmp->next;
+	/*check if the new top node is not NULL*/
 	if (*head != NULL)
 		(*head)->prev = NULL;
 	free(tmp);
