521c2ee48b7d5af8892cf439fb20e89e743ad2e9
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-01 18:07:12 -0800

    task1

--- task01.c
+++ task01.c
@@ -6,10 +6,12 @@
  */
 void handle_pint(stack_t **head, unsigned int ln)
 {
+	/*check if the stack is empty*/
 	if (head == NULL || *head == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", ln);
 		exit(EXIT_FAILURE);
 	}
+	/*print the value at the top of the stack*/
 	printf("%d\n", (*head)->n);
 }
