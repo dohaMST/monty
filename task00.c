186c2634728969e3aafb5e45aa24b61ae7a9b211
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-01 18:12:50 -0800

    task0

--- task00.c
+++ task00.c
@@ -12,7 +12,9 @@ void handle_push(stack_t **list, unsigned int ln)
 	stack_t *new_node = malloc(sizeof(stack_t));
 	int i, x;
 
+	/*check if the value is a valid integer*/
 	i = handle_check_num(val);
+	/*handle errors if the val is not an integer*/
 	if ((!i))
 	{
 		handle_free_list(&new_node);
@@ -25,7 +27,9 @@ void handle_push(stack_t **list, unsigned int ln)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
+	/*convert a str to an int*/
 	x = atoi(val);
+	/*assign values to the new node*/
 	new_node->n = x;
 	new_node->prev = NULL;
 	if (*list == NULL)
@@ -35,6 +39,7 @@ void handle_push(stack_t **list, unsigned int ln)
 		new_node->next = *list;
 		(*list)->prev = new_node;
 	}
+	/*set the head pointer to the new node*/
 	*list = new_node;
 }
 
@@ -48,9 +53,11 @@ void handle_pall(stack_t **list, unsigned int ln)
 	stack_t *tmp;
 
 	(void) ln;
+	/*check if the stack is empty*/
 	if (list == NULL)
 		exit(EXIT_FAILURE);
 	tmp = *list;
+	/*iterate through the stack and print each value*/
 	while (tmp != NULL)
 	{
 		printf("%d\n", tmp->n);
